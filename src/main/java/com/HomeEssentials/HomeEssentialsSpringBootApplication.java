3ea7e0c55187cf10c832dc716a8f865d3699ed7f
Spock95 <retkowskimarcin95@gmail.com>
2024-03-21 22:12:06 +0100

    Api[PUT,POST,DELETE] Blocked, Api-Endpoint-Documentation, General Documentation

--- src/main/java/com/HomeEssentials/HomeEssentialsSpringBootApplication.java
+++ src/main/java/com/HomeEssentials/HomeEssentialsSpringBootApplication.java
@@ -3,12 +3,13 @@ package com.HomeEssentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Klasa HomeEssentialsSpringBootApplication jest główną klasą aplikacji Spring Boot.
 @SpringBootApplication
 public class HomeEssentialsSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeEssentialsSpringBootApplication.class, args);
-
-	}
-
+ // Metoda main jest punktem wejścia do aplikacji.
+ public static void main(String[] args) {
+  // Metoda run uruchamia aplikację Spring Boot.
+  SpringApplication.run(HomeEssentialsSpringBootApplication.class, args);
+ }
 }
--- src/main/java/com/HomeEssentials/config/MyDataRestConfig.java
+++ src/main/java/com/HomeEssentials/config/MyDataRestConfig.java
@@ -1,12 +1,33 @@
 package com.HomeEssentials.config;
 
+import com.HomeEssentials.entity.Product;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+// Klasa MyDataRestConfig implementuje interfejs RepositoryRestConfigurer, który umożliwia dostosowanie konfiguracji Spring Data REST.
+@Configuration
 public class MyDataRestConfig  implements RepositoryRestConfigurer {
     @Override
+    // Metoda configureRepositoryRestConfiguration jest używana do konfiguracji CORS dla repozytoriów Spring Data REST.
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Wyłączenie metod PUT, POST i DELETE dla Product.
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // Wyłączenie metod PUT, POST i DELETE dla ProductCategory.
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
     }
 }
