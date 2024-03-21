496f91a994d6d43f151d0c29dc81edcf5240aaed
Spock95 <retkowskimarcin95@gmail.com>
2024-03-21 13:16:20 +0100

    Initial

---
+++ src/main/java/com/HomeEssentials/HomeEssentialsSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.HomeEssentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEssentialsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeEssentialsSpringBootApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/HomeEssentials/HomeEssentialsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeEssentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeEssentialsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
