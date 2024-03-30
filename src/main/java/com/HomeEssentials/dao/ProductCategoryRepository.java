43f9120928fe502f22cdab238ab6e3267957a17a
Spock95 <retkowskimarcin95@gmail.com>
2024-03-30 14:45:17 +0100

    pre swiateczny commit

--- src/main/java/com/HomeEssentials/dao/ProductCategoryRepository.java
+++ src/main/java/com/HomeEssentials/dao/ProductCategoryRepository.java
@@ -3,9 +3,11 @@ package com.HomeEssentials.dao;
 import com.HomeEssentials.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Interfejs ProductCategoryRepository rozszerza JpaRepository, co umożliwia korzystanie z gotowych metod do operacji CRUD.
 // Adnotacja @RepositoryRestResource umożliwia eksponowanie tego repozytorium jako zasobu REST.
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/HomeEssentials/dao/ProductRepository.java
+++ src/main/java/com/HomeEssentials/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.HomeEssentials.dao;
 
 import com.HomeEssentials.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Interfejs ProductRepository rozszerza JpaRepository, co umożliwia korzystanie z gotowych metod do operacji CRUD.
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 }
