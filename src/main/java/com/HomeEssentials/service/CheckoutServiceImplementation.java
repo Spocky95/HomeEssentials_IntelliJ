d3b9b4cbdcff440d508e4ae53e67b1cfa66d4429
Spock95 <retkowskimarcin95@gmail.com>
2024-04-27 00:03:08 +0200

    feat: Add CheckoutController for order processing

--- src/main/java/com/HomeEssentials/service/CheckoutServiceImplementation.java
+++ src/main/java/com/HomeEssentials/service/CheckoutServiceImplementation.java
@@ -3,8 +3,15 @@ package com.HomeEssentials.service;
 import com.HomeEssentials.dao.CustomerRepository;
 import com.HomeEssentials.dto.Purchase;
 import com.HomeEssentials.dto.PurchaseResponse;
+import com.HomeEssentials.entity.Customer;
+import com.HomeEssentials.entity.Order;
+import com.HomeEssentials.entity.OrderItem;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImplementation  implements CheckoutService {
@@ -16,7 +23,38 @@ public class CheckoutServiceImplementation  implements CheckoutService {
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber(order);
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(order::add);
+
+        // populate order with billingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        // save to the database
+        customerRepository.save(customer);
+
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber(Order order) {
+
+
+
+            return UUID.randomUUID().toString();
     }
 }
